cdb5065fdfd53dac27f3e185b39707ceee464ced
Daniel Busch <70059634+dakopen@users.noreply.github.com>
2021-10-31 18:37:11 +0100

    GUI Update

---
+++ GUI/AusspracheTrainerGui.py
@@ -0,0 +1,281 @@
+import random
+import time
+from PIL import Image, ImageDraw
+import PySimpleGUI as sg
+from base64 import b64encode
+import pyaudio
+import wave
+import io
+import threading
+from AusspracheTrainerSchnittstelle import aussprache_trainer
+import torch
+from AusspracheTrainerZusatz import SpeechRecognitionModel
+from IPAclass import *
+import os
+aufnehmen_bool = False
+finished = False
+path_zur_letzten_aufnahme = ""
+path_to_model = r"C:\Users\dakop\OneDrive\AusspracheTrainerKI.pt"
+
+
+def load_model():
+    """Lädt die AusspracheTrainerKI"""
+    # global AusspracheKI_preds, test_daten, path_to_model
+    print("[1/2] Model lädt...")
+    # Nach Vorlage von https://www.assemblyai.com/blog/end-to-end-speech-recognition-pytorch/
+    hparams = {
+        "n_cnn_layers": 3,
+        "n_rnn_layers": 5,
+        "rnn_dim": 512,
+        "n_class": 69,
+        "n_feats": 128,
+        "stride": 2,
+        "dropout": 0.1,
+        "batch_size": 1
+    }
+
+    use_cuda = torch.cuda.is_available()
+    if use_cuda:
+        logging.info("Cuda verfügbar --> GPU")
+    else:
+        logging.info("Cuda nicht verfügbar --> CPU")
+    torch.manual_seed(7)
+    device = torch.device("cuda" if use_cuda else "cpu")
+
+    model = SpeechRecognitionModel(
+        hparams['n_cnn_layers'], hparams['n_rnn_layers'], hparams['rnn_dim'],
+        hparams['n_class'], hparams['n_feats'], hparams['stride'], hparams['dropout']
+    ).to(device)
+    if not use_cuda:
+        checkpoint = torch.load(path_to_model, map_location="cpu")
+    else:
+        checkpoint = torch.load(path_to_model)
+
+    model.load_state_dict(checkpoint['state_dict'])
+    model.eval()
+    print("[1/2] Model hat geladen")
+    return model, device, use_cuda
+
+
+# https://github.com/PySimpleGUI/PySimpleGUI/issues/3412
+def round_btn(button_text, fill, text_color, tooltip=None, key=None,
+              pad=None, bind_return_key=False, button_width=None, visible=True):
+    multi = 5
+    btn_w = ((len(button_text) if button_width is None else button_width) * 5 + 20) * multi
+    height = 20 * multi
+    btn_img = Image.new('RGB', (btn_w, height), (sg.theme_background_color()))
+    d = ImageDraw.Draw(btn_img)
+    x0 = y0 = 0
+    radius = 10 * multi
+    d.ellipse((x0, y0, x0 + radius * 2, height), fill=fill)
+    d.ellipse((btn_w - radius * 2 - 1, y0, btn_w - 1, height), fill=fill)
+    d.rectangle((x0 + radius, y0, btn_w - radius, height), fill=fill)
+    data = io.BytesIO()
+    btn_img.thumbnail((btn_w // 3, height // 3), resample=Image.LANCZOS)
+    btn_img.save(data, format='png', quality=95)
+    btn_img = b64encode(data.getvalue())
+    return sg.Button(button_text=button_text, image_data=btn_img, button_color=(text_color, text_color),
+                     tooltip=tooltip, key=key, pad=pad, enable_events=False, size=(button_width, 1),
+                     bind_return_key=bind_return_key, border_width=0, visible=visible)
+
+#http://sebastiandahlgren.se/2014/06/27/running-a-method-as-a-background-thread-in-python/
+class AusspracheTrainer_Thread(object):
+
+    def __init__(self, target, path_to_audio, model, device, use_cuda):
+
+        thread = threading.Thread(target=self.run, args=(target, path_to_audio, model, device, use_cuda))  # alternativ hier direkt ausspracheTrainer reinmachen
+        thread.daemon = True                            # Daemonize thread
+        thread.start()
+
+    def run(self, target, path_to_audio, model, device, use_cuda):
+        try:
+            aussprache_trainer(target, path_to_audio, model, device, use_cuda)
+        except:
+            os.environ["overallscore"] = "-1"
+            window["fehlermeldung"].update("Google oder IBM haben deinen Satz nicht verstanden")
+
+
+class Update_Status_Thread(object):
+    def __init__(self, window, target, highscores):
+
+        thread = threading.Thread(target=self.run, args=(window, target, highscores))  # alternativ hier direkt ausspracheTrainer reinmachen
+        thread.daemon = True                            # Daemonize thread
+        thread.start()
+
+    def run(self, window, target, highscores):
+        while float(os.environ["overallscore"]) == 0.0:
+            pass
+
+        highscore = float(os.environ["overallscore"])
+        if float(os.environ["overallscore"]) > 0.0:
+
+            if target in highscores.keys():
+                if highscore >= highscores[target]:
+                    highscores[target] = highscore
+            else:
+                highscores[target] = highscore
+            with open(r"highscore.pickle", "wb") as highscores_speicherort:
+                pickle.dump(highscores, highscores_speicherort)
+            window["_highscore_"].update(highscore)
+        window["aufnehmen"].update("Weiter üben")
+        window["repeat"].update(visible=True)
+
+
+
+class Audio_aufnehmen(object):
+
+    def __init__(self):
+
+        thread = threading.Thread(target=self.run, args=())
+        thread.daemon = True                            # Daemonize thread
+        thread.start()                                  # Start the execution
+
+    def run(self):
+        global aufnehmen_bool, target, path_zur_letzten_aufnahme, finished
+        audio = pyaudio.PyAudio()
+        stream = audio.open(format=pyaudio.paInt16, channels=1, rate=48000, input=True, frames_per_buffer=768)
+        t_end = time.time() + 20
+        frames = []
+
+        # https://stackoverflow.com/questions/24374620/python-loop-to-run-for-certain-amount-of-seconds
+        while time.time() < t_end and aufnehmen_bool:
+            data = stream.read(768)
+            frames.append(data)
+        stream.stop_stream()
+        stream.close()
+        audio.terminate()
+        path_zur_letzten_aufnahme = Audio_aufnehmen.naming(self)
+        sound_file = wave.open(path_zur_letzten_aufnahme, "wb")
+        sound_file.setnchannels(1)
+        sound_file.setsampwidth(audio.get_sample_size(pyaudio.paInt16))
+        sound_file.setframerate(48000)
+        sound_file.writeframes(b''.join(frames))
+        sound_file.close()
+        finished = True
+
+    def naming(self):
+        index_number = 1
+        while True:
+            if os.path.isfile("Audios/Aufnahme_%d (%s).wav" % (index_number, "".join([e for e in target if
+                                                                               e not in [".", "*", "/", "\\", "[", "]",
+                                                                                         "(", ")", "{", "}", "?", "!", "="]]))):
+                index_number += 1
+
+            else:
+                return "Audios/Aufnahme_%d (%s).wav" % (index_number, "".join([e for e in target if
+                                                                               e not in [".", "*", "/", "\\", "[", "]",
+                                                                                         "(", ")", "{", "}", "?", "!", "="]]))
+
+
+def toggle_buttons(window, boolean):
+    window["_random_satz_"].update(disabled=boolean)
+    window["target"].update(disabled=boolean)
+    window["_DROPDOWN_"].update(disabled=boolean)
+
+try:
+    with open(r"highscores.pickle", "rb") as highscores_speicherort:
+        highscores = pickle.load(highscores_speicherort)
+except:
+    highscores = {}
+
+sg.theme('SandyBeach')
+
+layout = [
+    [sg.Text('AusspracheTrainer')],
+    [sg.Text('Satz ausdenken', size=(12, 1)), sg.InputText(key="target", size=(75, 1)), ],
+    [sg.Text('ODER', size=(12, 1)), sg.Combo(["/s/ - Satz", "/sch/ - Satz", "/ch1/ - Satz", "zufälligen Satz", "Wettbewerbssatz"], default_value="/s/ - Satz", size=(30, 1), key='_DROPDOWN_'), sg.Button("generieren", key="_random_satz_"), sg.Text(" " * 6), sg.Text('Höchster Score:'), sg.Text('-/-', key="_highscore_")],
+
+    [round_btn("AUFNEHMEN", sg.theme_button_color()[1], sg.theme_button_color()[0], button_width=30, key="aufnehmen"), round_btn("Wiederholen", sg.theme_button_color()[1], sg.theme_button_color()[0], button_width=20, key="repeat", visible=False)], #sg.FileBrowse(file_types=(("Audio Files", "*.wav"), ("All Files", "*.*")), size=(15, 1), key="browse")
+    [sg.Text('', key="anweisungen"), sg.Text('', key="fehlermeldung", text_color="red")]
+]
+
+window = sg.Window('AusspracheTrainer', layout)
+model, device, use_cuda = load_model()
+while True:
+    event, values = window.read()
+    if event in (sg.WIN_CLOSED, '_EXIT_'):
+        break
+    elif "_CONFIRM_" in event:
+        break
+    elif "_random_satz_" in event:
+        if values["_DROPDOWN_"] == "zufälligen Satz":
+            with open(r"C:\Users\dakop\OneDrive\Aussprache Trainer\Random_saetze.txt", "r", encoding="utf-8-sig") as random_saetze:
+                lines = random_saetze.readlines()
+                if values["target"] in lines:
+                    lines.remove(values["target"])
+                window["target"].update(random.choice(lines).strip())
+
+        elif values["_DROPDOWN_"] == "/s/ - Satz":
+            s_saetze = ["Susi sagte, dass sie gerne Salat mit Mais isst.", "Morgens isst Susanne gerne Müsli mit Nüssen.", "Auf der Insel scheint die Sonne übermäßig viel.", "Hans isst gerne Bratwurst mit Senf."]
+            if values["target"] in s_saetze:
+                s_saetze.remove(values["target"])
+            window["target"].update(random.choice(s_saetze))
+
+        elif values["_DROPDOWN_"] == "/sch/ - Satz":
+            sch_saetze = ["Am Strand bauen die Kinder mit dem Spielzeug und der Schaufel eine Sandburg.", "Die Schnecken hinterlassen eine Schleimspur auf der Straße.", "Auf der Schnellstraße herrscht schneller Straßenverkehr.", "Spreewaldgurken schmecken gut, sind aber im Spreewald am schmackhaftesten."]
+            if values["target"] in sch_saetze:
+                sch_saetze.remove(values["target"])
+            window["target"].update(random.choice(sch_saetze))
+        elif values["_DROPDOWN_"] == "/ch1/ - Satz":
+            ch1_saetze = ["Die Eichhörnchen sammeln Eicheln für ihren Wintervorrat.", "Ich möchte noch in die Kirche gehen.", "Es ist echt gefährlich sich im Auto nicht anzuschnallen.", "Ein Eichhörnchen ist leichter als ein Elch."]
+            if values["target"] in ch1_saetze:
+                ch1_saetze.remove(values["target"])
+            window["target"].update(random.choice(ch1_saetze))
+
+        elif values["_DROPDOWN_"] == "Wettbewerbssatz":
+            window["target"].update("Das Pferd isst keinen Gurkensalat.")
+
+
+        if values["target"] in highscores.keys():
+            window["_highscore_"].update(highscores[values["target"]])
+        else:
+            window["_highscore_"].update("-/-")
+
+    elif "aufnehmen" in event:
+        if window["aufnehmen"].get_text() == "Nehme auf... ":
+            aufnehmen_bool = False
+            time.sleep(1)
+            print(str(values["target"]), path_zur_letzten_aufnahme)
+            aussprache_trainer_thread = AusspracheTrainer_Thread(str(values["target"]), path_zur_letzten_aufnahme, model, device, use_cuda)
+            window["aufnehmen"].update("Aufnahme gespeichert ✓")
+            window["anweisungen"].update("")
+            window.refresh()
+            update_status_thread = Update_Status_Thread(window, str(values["target"]), highscores)
+
+        elif window["aufnehmen"].get_text() == "AUFNEHMEN":
+            target = values["target"]
+            target = str(target)
+            if target.strip() != "":
+                window["fehlermeldung"].update("")
+                toggle_buttons(window, True)
+                aufnehmen_bool = True
+                aufnehmen = Audio_aufnehmen()
+                window["aufnehmen"].update("Nehme auf... ")
+                window["repeat"].update(visible=False)
+            else:
+                window["fehlermeldung"].update("Bitte gib zuerst einen Satz ein!")
+
+        elif window["aufnehmen"].get_text() == "Aufnahme gespeichert ✓":
+            pass
+
+        elif window["aufnehmen"].get_text() == "Weiter üben":
+            toggle_buttons(window, False)
+            window["target"].update("")
+            window["_highscore_"].update("-/-")
+            window["aufnehmen"].update("AUFNEHMEN")
+            window["fehlermeldung"].update("")
+            window["repeat"].update(visible=False)
+
+
+    elif "repeat" in event:
+        window["repeat"].update(visible=False)
+        aufnehmen_bool = True
+        window["fehlermeldung"].update("")
+        aufnehmen = Audio_aufnehmen()
+        window["aufnehmen"].update("Nehme auf... ")
+
+
+
+window.close()
+with open(r"highscore.pickle", "wb") as highscores_speicherort:
+    pickle.dump(highscores, highscores_speicherort)
---
+++ GUI/Auswertung.py
@@ -0,0 +1,266 @@
+import re
+from termcolor import colored
+from Funktionen import einzelvergleich, bcolors
+import os
+sigmatismus_counter = 0
+rhotazismus_counter = 0
+chitismus_counter = 0
+schetismus_counter = 0
+
+
+class Sprachfehler:
+    # Laute, die Fehlerhaft werden sind Key; die Values repräsentieren, wonach es sich wahrscheinlich anhört
+    SIGMATISMUS = {"ʃ": ["f"], "s": ["f"], "ç": ["f"], "z": ["f"]}  # Auch als Lispeln bekannt
+    RHOTAZISMUS = {"ʀ": ["l"], "ɐ̯": ["l"], "r": ["l"]}
+    CHITISMUS = {"ç": ["ʃ", "s", "z"]}
+    SCHETISMUS = {"ʃ": ["s", "z", "ç"]}
+
+    # Weitere Sprachfehler einfügen
+
+
+def char_score_berechnen(target_output, char_scores, target):
+    """Errechnet basierend auf den farbigen Markierungen der vorangegangen Kalkulation den Score. Wenn ein Teil
+    des Targetsatzes gelb markiert (WARNING) ist - also sich fehlerhaft in der Prediction widerspiegelt - wird dieser
+    Buchstabe mit einem geringeren Score versehen. Dieser Buchstabe in den anderen Predictions auch markiert wurde,
+    wird der Score immer niedriger."""
+    # https://stackoverflow.com/questions/4664850/how-to-find-all-occurrences-of-a-substring
+    a = ([m.start() for m in re.finditer('\\033\[0m'.encode("utf-8").decode("utf-8"), target_output)])  # ENDC
+    b = ([m.start() for m in re.finditer('\\033\[93m'.encode("utf-8").decode("utf-8"), target_output)])  # WARNING
+    c = ([m.start() for m in re.finditer('\\033\[92m'.encode("utf-8").decode("utf-8"), target_output)])  # OKGREEN
+
+    char_zuordnung = {}
+    char_index_score = 0
+
+    for key in b:
+        char_zuordnung[key] = "b"
+    for key in c:
+        char_zuordnung[key] = "c"
+
+    for i, (key, char) in enumerate(sorted(char_zuordnung.items())):
+        start_at = key + 5
+        stop_at = a[i]
+        chars = target_output[start_at:stop_at]
+        if char == "b":
+            score = 0.33
+        else:  # char == "c":
+            score = 0
+        for j in range(len(chars)):
+            if target[char_index_score] == " " and chars[j] != " ":
+                char_index_score += 1
+            char_scores[char_index_score] = char_scores[char_index_score] - score
+            char_index_score += 1
+
+    return char_scores
+
+
+def chars_zu_viel(prediction):
+    """Sollten Teile der Prediction rot markiert sein (FAIL) - also überhaupt nicht gematched sein (bzw. überflüssig)
+    so wird hier der Score errechnet, der dadurch abgezogen werden muss. Er wird prozentual abgezogen."""
+    len_sequence = len(prediction)
+    minus_score = 0
+    a = ([m.start() for m in re.finditer('\\033\[0m'.encode("utf-8").decode("utf-8"), prediction)])  # ENDC
+    b = ([m.start() for m in re.finditer('\\033\[93m'.encode("utf-8").decode("utf-8"), prediction)])  # WARNING
+    c = ([m.start() for m in re.finditer('\\033\[92m'.encode("utf-8").decode("utf-8"), prediction)])  # OKGREEN
+    d = ([m.start() for m in re.finditer('\\033\[91m'.encode("utf-8").decode("utf-8"), prediction)])  # FAIL
+
+    char_zuordnung = {}
+    for key in b:
+        char_zuordnung[key] = "b"
+    for key in c:
+        char_zuordnung[key] = "c"
+    for key in d:
+        char_zuordnung[key] = "d"
+    for i, (key, char) in enumerate(sorted(char_zuordnung.items())):
+        if char == "d":
+
+            start_at = key + 5  # length of FAIL Sequence (\\033\[91m)
+            stop_at = a[i]
+            chars = prediction[start_at:stop_at]
+            chars = [i for i in chars if i != "_"]
+            score = (len(chars) / len_sequence) / 3  # Da es 3 Predictions gibt, sonst wäre zu viel Minus
+            minus_score += score
+    return minus_score
+
+def sprachfehler_finden(matchings, aussprachefehler):
+    sprachfehler_counter = 0
+    for matching in matchings.values():
+        if str(matching[0]).strip() != str(matching[1]).strip():  # unterscheiden sich
+            if any(sprachfehler for sprachfehler in aussprachefehler.keys() if any(laut for laut in sprachfehler if laut in matching[0])) and any(sprachfehler for sprachfehler in aussprachefehler.values() if any(laut for laut in sprachfehler if laut in matching[1])):
+                sprachfehler_counter += 1
+            elif any(sprachfehler for sprachfehler in aussprachefehler.values() if any(laut for laut in sprachfehler if laut in matching[0])) and any(sprachfehler for sprachfehler in aussprachefehler.keys() if any(laut for laut in sprachfehler if laut in matching[1])):
+                sprachfehler_counter += 1
+
+    return sprachfehler_counter
+
+def auswertung(target, predictions, ipa_zuordnungen):
+    global sigmatismus_counter, schetismus_counter, chitismus_counter, rhotazismus_counter
+    """Vergleicht den Targetsatz mit den 3 Predictions. Je nach Qualität der Aussprache wird der Satz in folgenden
+    Farben wieder ausgegeben:
+    Grün (perfekt), Lila (leicht unverständlich), Gelb (unverständlich), Rot (sehr unverständlich)
+    Außerdem wird die Aussprache auf Sprachfehler (zurzeit nur Sigmatimus (Lispeln)) überprüft.
+    """
+    predictions[2] = "".join([i for i in predictions[2] if i not in ["ˈ", "ˌ"]])
+    _, target_output, prediction_output, matchings = einzelvergleich(target, predictions[0])
+    _, target_output2, prediction_output2, matchings2 = einzelvergleich(target, predictions[1])
+    _, target_output3, prediction_output3, matchings3 = einzelvergleich(target, predictions[2])
+
+    char_scores = {}
+    for c, char in enumerate(target):
+        char_scores[c] = 1
+    char_scores = char_score_berechnen(target_output, char_scores, target)
+    char_scores = char_score_berechnen(target_output2, char_scores, target)
+    char_scores = char_score_berechnen(target_output3, char_scores, target)
+
+    print(bcolors.BOLD + "Target" + bcolors.ENDC)
+    print(target_output)
+    print(target_output2)
+    print(target_output3)
+    print(bcolors.BOLD + "Predictions" + bcolors.ENDC)
+    print(prediction_output)
+    print(prediction_output2)
+    print(prediction_output3)
+
+    # Leerzeichen sollten immer einen Score von 1 haben,
+    # da es mehrere Schreibweisen von Wörtern gibt: e gitarre / egitarre; zu hause / zuhause, etc.
+    for index in char_scores.keys():
+        if target[index] == " ":
+            char_scores[index] = 1
+
+    klartext_char_scores, klartext_target = ipa_zuordnen(ipa_zuordnungen, char_scores)
+    final_output = ""
+    for index, score in klartext_char_scores.items():
+        if score <= 0.25:  # durch Decimalverhalten ohne das Decimal Package wird 0.10 zu 0.09999999999999998
+            final_output += bcolors.FAIL
+        elif score <= 0.5:
+            final_output += bcolors.WARNING
+        elif score <= 0.75:
+            final_output += bcolors.INACCURACY
+        else:
+            final_output += bcolors.OKGREEN
+        final_output += klartext_target[index]
+        final_output += bcolors.ENDC
+
+    # Vorher: Analyse basierend auf der Lautschrift. Entkommentieren, wenn man Lautschrift gut lesen kann
+    r'''final_output = ""
+    for index, score in char_scores.items():
+        if score <= 0.1:  # durch Decimalverhalten ohne das Decimal Package wird 0.10 zu 0.09999999999999998
+            final_output += bcolors.FAIL
+        elif score <= 0.4:
+            final_output += bcolors.WARNING
+        elif score <= 0.7:
+            final_output += bcolors.INACCURACY
+        else:
+            final_output += bcolors.OKGREEN
+        final_output += target[index]
+        final_output += bcolors.ENDC'''
+
+    overall_score = sum(char_scores.values()) / len(char_scores)
+    print()
+    print(bcolors.BOLD + "Auswertung" + bcolors.ENDC)
+
+    print(final_output)
+    print()
+    overall_score -= (chars_zu_viel(prediction_output) + chars_zu_viel(prediction_output2) + chars_zu_viel(
+        prediction_output3))
+
+    matchings_liste = [matchings, matchings2, matchings3]
+    sigmatismus_counter += sum([sprachfehler_finden(match, Sprachfehler.SIGMATISMUS) for match in matchings_liste])
+    rhotazismus_counter += sum([sprachfehler_finden(match, Sprachfehler.RHOTAZISMUS) for match in matchings_liste])
+    schetismus_counter += sum([sprachfehler_finden(match, Sprachfehler.SCHETISMUS) for match in matchings_liste])
+    chitismus_counter += sum([sprachfehler_finden(match, Sprachfehler.CHITISMUS) for match in matchings_liste])
+
+
+    sigmatismus_occurences, rhotazismus_occurences, schetismus_occurences, chitismus_occurences = 0, 0 , 0, 0
+    sigmatismus_occurences += sum([str(target).count(key) for key in Sprachfehler.SIGMATISMUS.keys()])
+    rhotazismus_occurences += sum([str(target).count(key) for key in Sprachfehler.RHOTAZISMUS.keys()])
+    schetismus_occurences += sum([str(target).count(key) for key in Sprachfehler.SCHETISMUS.keys()])
+    chitismus_occurences += sum([str(target).count(key) for key in Sprachfehler.CHITISMUS.keys()])
+
+    # > 2 damit keine Nulldivision auftritt und es genug Aussagekraft hat
+    if sigmatismus_occurences > 2:
+        if sigmatismus_counter / sigmatismus_occurences > 0.7:
+            print(f"{bcolors.FAIL}Auffällig ungenaue Aussprache der Zischlaute "
+                  f"(Lispeln = {list(Sprachfehler.SIGMATISMUS.keys())} bzw. ['sch', 's', 'ch']).{bcolors.ENDC}")
+        elif sigmatismus_counter / sigmatismus_occurences > 0.4:
+            print(f"{bcolors.WARNING}Überdurchschnittlich ungenaue Aussprache der Zischlaute "
+                  f"(Lispeln = {list(Sprachfehler.SIGMATISMUS.keys())}  bzw. ['sch', 's', 'ch']). {bcolors.ENDC}")
+
+    if rhotazismus_occurences > 2:
+        if rhotazismus_counter / rhotazismus_occurences > 0.7:
+            print(f"{bcolors.FAIL}Auffällig ungenaue Aussprache beim R "
+                  f"(Rhotazismus = {list(Sprachfehler.RHOTAZISMUS.keys())}).{bcolors.ENDC}")
+        elif rhotazismus_counter / rhotazismus_occurences > 0.4:
+            print(f"{bcolors.WARNING}Überdurchschnittlich ungenaue Aussprache beim R "
+                  f"(Rhotazismus = {list(Sprachfehler.RHOTAZISMUS.keys())}). {bcolors.ENDC}")
+
+    if schetismus_occurences > 2:
+        if schetismus_counter / schetismus_occurences > 0.7:
+            print(f"{bcolors.FAIL}Auffällig ungenaue Aussprache der Zischlaute "
+                  f"(Schetismus = {list(Sprachfehler.SCHETISMUS.keys())}).{bcolors.ENDC}")
+        elif schetismus_counter / schetismus_occurences > 0.4:
+            print(f"{bcolors.WARNING}Überdurchschnittlich ungenaue Aussprache der Zischlaute "
+                  f"(Schetismus = {list(Sprachfehler.SCHETISMUS.keys())}). {bcolors.ENDC}")
+
+    if chitismus_occurences > 2:
+        if chitismus_counter / chitismus_occurences > 0.7:
+            print(f"{bcolors.FAIL}Auffällig ungenaue Aussprache der CH-Laute "
+                  f"(Chitismus = {list(Sprachfehler.CHITISMUS.keys())}).{bcolors.ENDC}")
+        elif chitismus_counter / chitismus_occurences > 0.4:
+            print(f"{bcolors.WARNING}Überdurchschnittlich ungenaue Aussprache der CH-Laute "
+                  f"(Chitismus = {list(Sprachfehler.CHITISMUS.keys())}). {bcolors.ENDC}")
+
+    bewertung_des_scores = "Deine Aussprache ist "
+    if overall_score > 0.95:
+        print(bewertung_des_scores + "exzellent | Score:", overall_score)
+    elif overall_score > 0.90:
+        print(bewertung_des_scores + "sehr gut | Score:", overall_score)
+    elif overall_score > 0.85:
+        print(bewertung_des_scores + "gut | Score:", overall_score)
+    elif overall_score > 0.80:
+        print(bewertung_des_scores + "verständlich | Score:", overall_score)
+    elif overall_score > 0.75:
+        print(bewertung_des_scores + "meistens verständlich | Score:", overall_score)
+    elif overall_score > 0.70:
+        print(bewertung_des_scores + "oft verständlich | Score:", overall_score)
+    elif overall_score > 0.65:
+        print(bewertung_des_scores + "für andere manchmal unverständlich | Score:", overall_score)
+    elif overall_score > 0.60:
+        print(bewertung_des_scores + "für andere häufig unverständlich | Score:", overall_score)
+    elif overall_score > 0.55:
+        print(bewertung_des_scores + "für andere sehr häufig unverständlich | Score:", overall_score)
+    elif overall_score > 0.50:
+        print(bewertung_des_scores + "für andere schwierig zu verstehen | Score:", overall_score)
+    elif overall_score > 0.45:
+        print(bewertung_des_scores + "für andere sehr schwierig zu verstehen | Score:", overall_score)
+    elif overall_score > 0.30:
+        print(bewertung_des_scores + "für andere fast nicht zu verstehen | Score:", overall_score)
+    elif overall_score > 0.20:
+        print(bewertung_des_scores + "für andere gar nicht zu verstehen | Score:", overall_score)
+    else:
+        print(bewertung_des_scores + "grundsätzlich unverständlich | Score:", overall_score)
+
+    if overall_score < 0.7:
+        print("Wir empfehlen dringend Übungen zur Verbesserung der Aussprache zu machen.")
+    if overall_score < 0.55:
+        print("Außerdem könnte eine logopädische Praxis Dir bei diesem Thema behilflich sein.")
+    os.environ['overallscore'] = str(round(overall_score, 3))
+
+
+def ipa_zuordnen(zuordnungen, char_scores):
+    """IPA wird in Klartext zurück umgeformt, allerdings werden die vorher für
+    die Lautschrift errechneten Werte einbezogen.
+    Input: Zuordnungsliste, Char_scores
+    Output: Neue Char_scores für das Klartext_target (da andere Indexe), Klartext_target"""
+    buchstaben_counter = 0
+    new_char_scores = {}
+    ipa_buchstaben_counter = 0
+    for buchstabenpaar in zuordnungen:
+        klartext = buchstabenpaar[0]
+        ipa = buchstabenpaar[1]
+        durchschitt = sum([char_scores[i+buchstaben_counter] for i in range(len(ipa))])/len(ipa)
+        buchstaben_counter += len(ipa)
+        for x in range(len(klartext)):
+            new_char_scores[ipa_buchstaben_counter] = durchschitt
+            ipa_buchstaben_counter += 1
+    klartext_target = "".join([i[0] for i in zuordnungen])
+    return new_char_scores, klartext_target
